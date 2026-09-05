05287e69f31f4b2792971673e9106edf15cd8354
sethwebster <sethwebster@gmail.com>
2026-09-05 17:54:50 -0400

    Add Italian lesson 2026-09-05

--- site-assets/grader.js
+++ site-assets/grader.js
@@ -11,7 +11,8 @@
     "2026-08-29": ["Quale frase è corretta?", ["Le ragazze si sono sedute sul divano.", "Le ragazze hanno sedute sul divano.", "Le ragazze si è seduta sul divano."], "a"],
     "2026-08-30": ["Quale frase è corretta?", ["Emma e Paolo hanno arrivato insieme.", "Emma e Paolo sono arrivate insieme.", "Emma e Paolo sono arrivati insieme."], "c"],
     "2026-08-31": ["Quale frase è corretta?", ["Sara ha uscita dalla biblioteca.", "Sara è uscita dalla biblioteca.", "Sara è uscito dalla biblioteca."], "b"],
-    "2026-09-01": ["Quale frase crea correttamente lo sfondo?", ["Ha fatto caldo e i bambini hanno giocato.", "Faceva caldo e i bambini giocavano.", "Faceva caldo e i bambini hanno giocavano."], "b"]
+    "2026-09-01": ["Quale frase crea correttamente lo sfondo?", ["Ha fatto caldo e i bambini hanno giocato.", "Faceva caldo e i bambini giocavano.", "Faceva caldo e i bambini hanno giocavano."], "b"],
+    "2026-09-05": ["Quale frase presenta correttamente un'azione interrotta?", ["Mentre abbiamo cenato, la luce andava via.", "Mentre cenavamo, la luce è andata via.", "Mentre cenavamo, la luce andava via improvvisamente."], "b"]
   };
 
   const fallback = [
